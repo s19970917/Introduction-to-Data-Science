e2c26e69d97ef9eea8b0037772613033cc7c760a
wen-yu <43664514+s19970917@users.noreply.github.com>
2018-11-14 21:42:00 +0800

    Add files via upload

---
+++ homework/R語言資料法.R
@@ -0,0 +1,30 @@
+8 > 7 # 耞 8 琌??? 7
+8 < 7 # 耞 8 琌??? 7
+8 >= 7 # 耞 8 琌???单 7
+8 <= 7 # 耞 8 琌???单 7
+8 == 7 # 耞 8 琌单 7
+8 != 7 # 耞 8 琌ぃ单 7
+7 %in% c(8, 7) # 耞 7 琌??? c(8, 7) ぇい
+
+my_height <- 165
+my_weight <- 51
+bmi <- (51 / (165/100)^2)
+my_name <- "ゅ抠"
+
+beyond_start <- as.Date("1983-12-31")
+is.integer("1983-12-31L")
+
+beyond_start <- as.Date("1983-12-31")
+days_diff <- 1983-12-31 - 2018-11-14 # 璸衡ぱ计畉禯
+years_diff <- 1033 / 365 # 埃 365 传衡Θ
+
+major_quake_time <- "19999る21ら147だ16"
+first_aftershock_time <- "19999る21ら157だ15"
+major_quake_time <- as.POSIXct(1999-9-21 01:47:16)
+first_aftershock_time <- as.POSIXct(1999-9-21 01:57:15)
+
+weekdays <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+favorite_day <- weekdays["Monday"]
+weekdays <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+not_blue_monday <- weekdays != "Monday"
+without_monday <- weekdays["Monday"]
